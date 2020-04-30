d69246631166629ec43f9230a7f4c6b0c61e7972
bohdantverdyi <ownedd777@gmail.com>
2020-04-30 18:55:12 +0300

    GSIAAC-26 Add type for variables

--- variables.tf
+++ variables.tf
@@ -1,22 +1,28 @@
 variable "name" {
+  type = string
   description = "(Required) Deployment name"
 }
 variable "namespace" {
+  type = string
   description = "(Optional) K8S namespace where deploy app"
   default = "default"
 }
 variable "image" {
+  type = string
   description = "(Required) Docker image for app"
 }
 variable "volume_nfs" {
+  type = list(object({path_on_nfs = string, nfs_endpoint = string, volume_name = string}))
   description = "(Optional) Attach NFS"
   default = []
 }
 variable "volume_host_path" {
+  type = list(object({}))
   description = "(Optional) Attach a file or directory from the host node’s filesystem"
   default = []
 }
 variable "volume_config_map" {
+  type = list(object({mode = string, name = string, volume_name = string}))
   description = "(Optional) The data stored in a ConfigMap object can be referenced in a volume of type configMap and then consumed by containerized applications running in a Pod"
   default = []
 }
@@ -29,14 +35,17 @@ variable "volume_mount" {
   default = []
 }
 variable "env" {
+  type = list(object({name = string, value = string}))
   description = "(Optional) Add environment variables to pods."
   default = []
 }
 variable "env_field" {
+  type = list(object({name = string, field_path = string}))
   description = "(Optional) Get field from k8s and add as environment variables to pods"
   default = []
 }
 variable "hosts" {
+  type = list(object({hostname = string, ip = string}))
   description = "(Optional) Add /etc/hosts records to pods"
   default = []
 }
@@ -56,29 +65,36 @@ variable "custom_labels" {
   default = null
 }
 variable "args" {
+  type = list(string)
   description = "(Optional) Arguments to the entrypoint."
   default = []
 }
 variable "tty" {
+  type = bool
   default = true
 }
 variable "service_account_token" {
+  type = bool
   description = "Indicates whether a service account token should be automatically mounted"
   default = null
 }
 variable "service_account_name" {
+  type = string
   description = "(Optional) Is the name of the ServiceAccount to use to run this pod"
   default = null
 }
 variable "restart_policy" {
+  type = string
   description = "Restart policy for all containers within the pod. One of Always, OnFailure, Never"
   default = "Always"
 }
 variable "replicas" {
+  type = number
   description = "(Optional) Count of pods"
-  default = "1"
+  default = 1
 }
 variable "min_ready_seconds" {
+  type = number
   description = "(Optional) Field that specifies the minimum number of seconds for which a newly created Pod should be ready without any of its containers crashing, for it to be considered available  "
   default = null
 }
