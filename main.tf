d4c7217d5e07c5a2b2e3976f16e3525d2025071d
bohdantverdyi <ownedd777@gmail.com>
2020-08-25 19:55:18 +0300

    Refactoring

--- main.tf
+++ main.tf
@@ -1,49 +1,59 @@
 resource "kubernetes_deployment" "deploy_app" {
   metadata {
-    name = var.name
+    name      = var.name
     namespace = var.namespace
-    labels = local.labels
+    labels    = local.labels
   }
+
   spec {
     min_ready_seconds = var.min_ready_seconds
-    replicas = var.replicas
+    replicas          = var.replicas
     selector {
       match_labels = local.labels
     }
+
     template {
+
       metadata {
         labels = local.labels
       }
+
       spec {
+
         dynamic "security_context" {
           for_each = var.security_context
           content {
-            fs_group = lookup(security_context.value, "fs_group", null )
-            run_as_group = lookup(security_context.value, "group_id", null)
-            run_as_user = lookup(security_context.value, "user_id", null)
+            fs_group        = lookup(security_context.value, "fs_group", null )
+            run_as_group    = lookup(security_context.value, "group_id", null)
+            run_as_user     = lookup(security_context.value, "user_id", null)
             run_as_non_root = lookup(security_context.value, "as_non_root", null)
 
           }
         }
-        service_account_name = var.service_account_name
+
+        service_account_name            = var.service_account_name
         automount_service_account_token = var.service_account_token
+
         container {
-          image = var.image
-          name = var.name
-          args = var.args
-          command = var.command
+
+          image             = var.image
+          name              = var.name
+          args              = var.args
+          command           = var.command
           image_pull_policy = var.image_pull_policy
+
           dynamic "env" {
             for_each = var.env
             content {
-              name = env.value.name
-              value = env.value.value
+              name   = env.value.name
+              value  = env.value.value
             }
           }
+
           dynamic "env" {
             for_each = var.env_field
             content {
-              name = env.value.name
+              name   = env.value.name
               value_from {
                 field_ref {
                   field_path = env.value.field_path
@@ -51,212 +61,244 @@ resource "kubernetes_deployment" "deploy_app" {
               }
             }
           }
+
           dynamic "env" {
             for_each = var.env_secret
             content {
-              name = env.value.name
+              name   = env.value.name
               value_from {
                 secret_key_ref {
                   name = env.value.secret_name
-                  key = env.value.secret_key
+                  key  = env.value.secret_key
                 }
               }
             }
           }
+
           dynamic "security_context" {
             for_each = var.security_context_capabilities
             content {
               allow_privilege_escalation = false
               capabilities {
-                add = lookup(security_context.value, "add", [] )
+                add  = lookup(security_context.value, "add", [] )
                 drop = lookup(security_context.value, "drop", [] )
               }
             }
           }
+
           dynamic "resources" {
             for_each = var.resources
             content {
               requests {
-                cpu = lookup(resources.value, "request_cpu", null)
+                cpu    = lookup(resources.value, "request_cpu", null)
                 memory = lookup(resources.value, "request_memory", null)
               }
               limits {
-                cpu = lookup(resources.value, "limit_cpu", null)
+                cpu    = lookup(resources.value, "limit_cpu", null)
                 memory = lookup(resources.value, "limit_memory", null)
               }
             }
           }
+
           dynamic "port" {
             for_each = var.internal_port
             content {
               container_port = port.value.internal_port
-              name = substr(port.value.name, 0, 14)
-              host_port = lookup(port.value, "host_port", null)
+              name           = substr(port.value.name, 0, 14)
+              host_port      = lookup(port.value, "host_port", null)
             }
           }
+
           dynamic "volume_mount" {
             for_each = var.volume_mount
             content {
               mount_path = volume_mount.value.mount_path
-              sub_path = lookup(volume_mount.value, "sub_path", null)
-              name = volume_mount.value.volume_name
-              read_only = lookup(volume_mount.value, "read_only", false)
+              sub_path   = lookup(volume_mount.value, "sub_path", null)
+              name       = volume_mount.value.volume_name
+              read_only  = lookup(volume_mount.value, "read_only", false)
             }
           }
+
           dynamic "liveness_probe" {
             for_each = var.liveness_probe
             content {
-              failure_threshold = lookup(liveness_probe.value, "failure_threshold", null)
+              failure_threshold     = lookup(liveness_probe.value, "failure_threshold", null)
               initial_delay_seconds = lookup(liveness_probe.value, "initial_delay_seconds", null)
-              period_seconds = lookup(liveness_probe.value, "period_seconds", null)
-              success_threshold = lookup(liveness_probe.value, "success_threshold", null)
-              timeout_seconds = lookup(liveness_probe.value, "timeout_seconds", null)
+              period_seconds        = lookup(liveness_probe.value, "period_seconds", null)
+              success_threshold     = lookup(liveness_probe.value, "success_threshold", null)
+              timeout_seconds       = lookup(liveness_probe.value, "timeout_seconds", null)
+
               dynamic "http_get"{
                 for_each = lookup(liveness_probe.value, "http_get", [])
                 content {
-                  path = lookup(http_get.value, "path", null)
-                  port = lookup(http_get.value, "port", null)
+                  path   = lookup(http_get.value, "path", null)
+                  port   = lookup(http_get.value, "port", null)
                   scheme = lookup(http_get.value, "scheme", null)
-                  host = lookup(http_get.value, "host", null)
+                  host   = lookup(http_get.value, "host", null)
 
                   dynamic "http_header" {
-                    for_each = lookup(http_get.value, "header_name", [])
+                    for_each = lookup(http_get.value, "http_header", [])
                     content {
-                      name = lookup(http_get.value, "header_name", null)
-                      value = lookup(http_get.value, "header_value", null)
+                      name   = lookup(http_header.value, "name", null)
+                      value  = lookup(http_header.value, "value", null)
                     }
                   }
                 }
               }
+
               dynamic "tcp_socket" {
                 for_each = lookup(liveness_probe.value, "tcp_socket", null) == null ? [] : [{}]
                 content {
-                  port = liveness_probe.value.tcp_socket_port
+                  port   = liveness_probe.value.tcp_socket_port
                 }
               }
+
             }
           }
+
           dynamic "readiness_probe" {
             for_each = var.readiness_probe
             content {
-              failure_threshold = lookup(readiness_probe.value, "failure_threshold", null)
+              failure_threshold     = lookup(readiness_probe.value, "failure_threshold", null)
               initial_delay_seconds = lookup(readiness_probe.value, "initial_delay_seconds", null)
-              period_seconds =  lookup(readiness_probe.value, "period_seconds", null)
-              success_threshold = lookup(readiness_probe.value, "success_threshold", null)
-              timeout_seconds = lookup(readiness_probe.value, "timeout_seconds", null)
+              period_seconds        =  lookup(readiness_probe.value, "period_seconds", null)
+              success_threshold     = lookup(readiness_probe.value, "success_threshold", null)
+              timeout_seconds       = lookup(readiness_probe.value, "timeout_seconds", null)
+
               dynamic "http_get"{
                 for_each = lookup(readiness_probe.value, "http_get", [])
                 content {
-                  path = lookup(http_get.value, "path", null)
-                  port = lookup(http_get.value, "port", null)
+                  path   = lookup(http_get.value, "path", null)
+                  port   = lookup(http_get.value, "port", null)
                   scheme = lookup(http_get.value, "scheme", null)
-                  host = lookup(http_get.value, "host", null)
+                  host   = lookup(http_get.value, "host", null)
 
                   dynamic "http_header" {
-                    for_each = lookup(http_get.value, "header_name", [])
+                    for_each = lookup(http_get.value, "http_header", [])
                     content {
-                      name = lookup(http_get.value, "header_name", null)
-                      value = lookup(http_get.value, "header_value", null)
+                      name   = lookup(http_header.value, "name", null)
+                      value  = lookup(http_header.value, "value", null)
                     }
                   }
+
                 }
               }
+
               dynamic "tcp_socket" {
                 for_each = lookup(readiness_probe.value, "tcp_socket", null) == null ? [] : [{}]
                 content {
-                  port = readiness_probe.value.tcp_socket_port
+                  port   = readiness_probe.value.tcp_socket_port
                 }
               }
+
             }
           }
+
           dynamic "lifecycle"{
             for_each = var.lifecycle_events
             content {
+
               dynamic "pre_stop"{
                 for_each = lookup(lifecycle.value, "pre_stop", [])
+
                 content {
                   exec {
                     command = lookup(pre_stop.value, "exec_command", null)
                   }
+
                   dynamic "http_get"{
                     for_each = lookup(pre_stop.value, "http_get", [])
                     content {
-                      path = lookup(http_get.value, "path", null)
-                      port = lookup(http_get.value, "port", null)
+                      path   = lookup(http_get.value, "path", null)
+                      port   = lookup(http_get.value, "port", null)
                       scheme = lookup(http_get.value, "scheme", null)
-                      host = lookup(http_get.value, "host", null)
+                      host   = lookup(http_get.value, "host", null)
 
                       dynamic "http_header" {
-                        for_each = lookup(http_get.value, "header_name", [])
+                        for_each = lookup(http_get.value, "http_header", [])
                         content {
-                          name = lookup(http_get.value, "header_name", null)
-                          value = lookup(http_get.value, "header_value", null)
+                          name   = lookup(http_header.value, "name", null)
+                          value  = lookup(http_header.value, "value", null)
                         }
                       }
                     }
                   }
+
                   dynamic "tcp_socket" {
                     for_each = lookup(lifecycle.value, "tcp_socket", null) == null ? [] : [{}]
                     content {
-                      port = lifecycle.value.tcp_socket_port
+                      port   = lifecycle.value.tcp_socket_port
                     }
                   }
+
                 }
               }
+
               dynamic "post_start"{
                 for_each = lookup(lifecycle.value, "post_start", [])
+
                 content {
                   exec {
                     command = lookup(post_start.value, "exec_command", null)
                   }
+
                   dynamic "http_get"{
                     for_each = lookup(post_start.value, "http_get", [])
                     content {
-                      path = lookup(http_get.value, "path", null)
-                      port = lookup(http_get.value, "port", null)
+                      path   = lookup(http_get.value, "path", null)
+                      port   = lookup(http_get.value, "port", null)
                       scheme = lookup(http_get.value, "scheme", null)
-                      host = lookup(http_get.value, "host", null)
+                      host   = lookup(http_get.value, "host", null)
 
                       dynamic "http_header" {
-                        for_each = lookup(http_get.value, "header_name", [])
+                        for_each = lookup(http_get.value, "http_header", [])
                         content {
-                          name = lookup(http_get.value, "header_name", null)
-                          value = lookup(http_get.value, "header_value", null)
+                          name   = lookup(http_header.value, "name", null)
+                          value  = lookup(http_header.value, "value", null)
                         }
                       }
+
                     }
                   }
+
                   dynamic "tcp_socket" {
                     for_each = lookup(lifecycle.value, "tcp_socket", null) == null ? [] : [{}]
                     content {
-                      port = lifecycle.value.tcp_socket_port
+                      port   = lifecycle.value.tcp_socket_port
                     }
                   }
+
                 }
+
               }
+
             }
           }
+
           tty = var.tty
         }
+
         node_selector = var.node_selector
         dynamic "host_aliases"{
           iterator = hosts
           for_each = var.hosts
           content {
             hostnames = hosts.value.hostname
-            ip = hosts.value.ip
+            ip        = hosts.value.ip
           }
         }
+
         dynamic "volume" {
-          for_each = var.volume_nfs
+          for_each   = var.volume_nfs
           content {
             nfs {
-              path = volume.value.path_on_nfs
+              path   = volume.value.path_on_nfs
               server = volume.value.nfs_endpoint
             }
-            name = volume.value.volume_name
+            name     = volume.value.volume_name
           }
         }
+
         dynamic "volume" {
           for_each = var.volume_host_path
           content {
@@ -264,44 +306,49 @@ resource "kubernetes_deployment" "deploy_app" {
               path = volume.value.path_on_node
               type = lookup(volume.value, "type", null )
             }
-            name = volume.value.volume_name
+            name   = volume.value.volume_name
           }
         }
+
         dynamic "volume" {
-          for_each = var.volume_config_map
+          for_each         = var.volume_config_map
           content {
             config_map {
               default_mode = volume.value.mode
-              name = volume.value.name
+              name         = volume.value.name
             }
-            name = volume.value.volume_name
+            name           = volume.value.volume_name
           }
         }
+
         dynamic "volume" {
-          for_each = var.volume_gce_disk
+          for_each      = var.volume_gce_disk
           content {
             gce_persistent_disk {
-              pd_name = volume.value.gce_disk
-              fs_type = lookup(volume.value, "fs_type", null )
+              pd_name   = volume.value.gce_disk
+              fs_type   = lookup(volume.value, "fs_type", null )
               partition = lookup(volume.value, "partition", null )
               read_only = lookup(volume.value, "read_only", null)
             }
-            name = volume.value.volume_name
+            name        = volume.value.volume_name
           }
         }
+
         dynamic "volume" {
-          for_each = var.volume_aws_disk
+          for_each      = var.volume_aws_disk
           content {
             aws_elastic_block_store {
-              fs_type = lookup(volume.value, "fs_type", null )
+              fs_type   = lookup(volume.value, "fs_type", null )
               partition = lookup(volume.value, "partition", null )
               read_only = lookup(volume.value, "read_only", null)
               volume_id = volume.value.volume_id
             }
-            name = volume.value.volume_name
+            name        = volume.value.volume_name
           }
         }
+
         restart_policy = var.restart_policy
+
       }
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,143 +1,141 @@
 variable "name" {
-  type = string
+  type        = string
   description = "(Required) Deployment name"
 }
 variable "namespace" {
-  type = string
+  type        = string
   description = "(Optional) K8S namespace where deploy app"
-  default = "default"
+  default     = "default"
 }
 variable "image" {
-  type = string
+  type        = string
   description = "(Required) Docker image for app"
 }
 variable "volume_nfs" {
-  type = list(object({path_on_nfs = string, nfs_endpoint = string, volume_name = string}))
+  type        = list(object({path_on_nfs = string, nfs_endpoint = string, volume_name = string}))
   description = "(Optional) Attach NFS"
-  default = []
+  default     = []
 }
 variable "volume_host_path" {
   description = "(Optional) Attach a file or directory from the host node’s filesystem"
-  default = []
+  default     = []
 }
 variable "volume_config_map" {
-  type = list(object({mode = string, name = string, volume_name = string}))
+  type        = list(object({mode = string, name = string, volume_name = string}))
   description = "(Optional) The data stored in a ConfigMap object can be referenced in a volume of type configMap and then consumed by containerized applications running in a Pod"
-  default = []
+  default     = []
 }
 variable "volume_gce_disk" {
   description = "(Optional) Create volume from google disk to pod"
-  default = []
+  default     = []
 }
 variable "volume_aws_disk" {
   description = "(Optional) Create volume from aws disk to pod"
-  default = []
+  default     = []
 }
 variable "volume_mount" {
   description = "(Optional) Mount path from pods to volume"
-  default = []
+  default     = []
 }
 variable "env" {
-  type = list(object({name = string, value = string}))
+  type        = list(object({name = string, value = string}))
   description = "(Optional) Add environment variables to pods."
-  default = []
+  default     = []
 }
 variable "env_field" {
-  type = list(object({name = string, field_path = string}))
+  type        = list(object({name = string, field_path = string}))
   description = "(Optional) Get field from k8s and add as environment variables to pods"
-  default = []
+  default     = []
 }
 variable "hosts" {
-  type = list(object({hostname = string, ip = string}))
+  type        = list(object({hostname = string, ip = string}))
   description = "(Optional) Add /etc/hosts records to pods"
-  default = []
+  default     = []
 }
 variable "internal_port" {
   description = "(Optional) Expose port in pods"
-  default = []
+  default     = []
 }
 variable "security_context" {
   description = "(Optional) Set startup user_id, when pods start"
-  default = []
+  default     = []
 }
 
 variable "custom_labels" {
   description = "(Optional) Add custom label to pods"
-  default = null
+  default     = null
 }
 variable "args" {
-  type = list(string)
+  type        = list(string)
   description = "(Optional) Arguments to the entrypoint."
-  default = []
+  default     = []
 }
 
 variable "command" {
   description = " (Optional) Entrypoint array. Not executed within a shell. "
-  default = []
+  default     = []
 }
 
 variable "tty" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 variable "service_account_token" {
-  type = bool
+  type        = bool
   description = "Indicates whether a service account token should be automatically mounted"
-  default = null
+  default     = null
 }
 variable "service_account_name" {
-  type = string
+  type        = string
   description = "(Optional) Is the name of the ServiceAccount to use to run this pod"
-  default = null
+  default     = null
 }
-
 variable "image_pull_policy" {
-  default = "IfNotPresent" // Always, Never or empty
+  default     = "IfNotPresent" // Always, Never or empty
   description = "he default pull policy is IfNotPresent which causes the Kubelet to skip pulling an image if it already exists. If you would like to always force a pull, you can do one of the following"
 }
-
 variable "restart_policy" {
-  type = string
+  type        = string
   description = "Restart policy for all containers within the pod. One of Always, OnFailure, Never"
-  default = "Always"
+  default     = "Always"
 }
 variable "replicas" {
-  type = number
+  type        = number
   description = "(Optional) Count of pods"
-  default = 1
+  default     = 1
 }
 variable "min_ready_seconds" {
-  type = number
+  type        = number
   description = "(Optional) Field that specifies the minimum number of seconds for which a newly created Pod should be ready without any of its containers crashing, for it to be considered available  "
-  default = null
+  default     = null
 }
 variable "resources" {
   description = "(Optional) Limit resources by cpu or memory for pods"
-  default = []
+  default     = []
 }
 variable "liveness_probe" {
   description = "(Optional) Periodic probe of container liveness. Container will be restarted if the probe fails. Cannot be updated. "
-  default = []
+  default     = []
 }
 variable "readiness_probe" {
   description = "(Optional) Periodic probe of container service readiness. Container will be removed from service endpoints if the probe fails. Cannot be updated. "
-  default = []
+  default     = []
 }
 variable "lifecycle_events" {
   description = "(Optional) Actions that the management system should take in response to container lifecycle events"
-  default = []
+  default     = []
 }
 variable "node_selector" {
   description = "(Optional) Specify node selector for pod"
-  type = map(string)
-  default = null
+  type        = map(string)
+  default     = null
 }
 variable "security_context_capabilities" {
   description = "(Optional) Security context in pod. Only capabilities."
-  default = []
+  default     = []
 }
 variable "env_secret" {
   description = "(Optional) Get secret keys from k8s and add as environment variables to pods"
-  type = list(object({name = string, secret_name = string, secret_key = string}))
-  default = []
+  type        = list(object({name = string, secret_name = string, secret_key = string}))
+  default     = []
 }
